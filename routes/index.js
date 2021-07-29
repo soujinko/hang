b8357c7bdb5afbcf0a50ced28d56822c93c851da
soujinko <soujinko@naver.com>
2021-07-30 01:42:33 +0900

    api index update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import mainRouter from "./main.js";
+import likeRouter from "./like.js";
 
 const router = express.Router();
+router.use("/main", mainRouter);
+router.use("/like", likeRouter);
 
 export default router;

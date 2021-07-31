d731074ea4dd6ec51b8ae94e04e6bbf8b03685c2
soujinko <soujinko@naver.com>
2021-07-31 16:09:57 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import mainRouter from "./main.js";
 import likeRouter from "./like.js";
+import usersRouter from "./users.js";
 import userRouter from "./user.js";
 
 const router = express.Router();
 router.use("/main", mainRouter);
 router.use("/like", likeRouter);
 router.use("/user", userRouter);
-
 router.use("/users", usersRouter);
 
 export default router;

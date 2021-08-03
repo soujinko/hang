50905e7ff4212e38fef3b915b3e27269a4071620
soujinko <soujinko@naver.com>
2021-08-03 18:10:42 +0900

    https 적용

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ import http from "http";
 import passport from "passport";
 import passportConfig from "./passport/passport.js";
 import swaggerDocs from "./config/swagger_config.js";
+import fs from "fs";
+import https from "https";
 
 const app = express();
-const server = http.createServer(app);
 
 dotenv.config();
 
 const corsOption = {
-  origin: "http://localhost:3000",
-  // Credential: true,
+  origin: "https://localhost:3000",
+  credentials: true,
   optionSuccessStatus: 200,
 };
-
+app.use(express.static("public"));
 app.use(cors(corsOption));
 app.use(logger("dev"));
 app.use(helmet());
@@ -33,12 +34,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 passportConfig();
 
+app.get("/", (req, res, next) => {
+  console.log("쿠리쿠리", req.cookies);
+  res.send("hello world");
+});
 app.use("/api", router);
 app.use("/docs", swaggerDocs);
 app.use(errorHandlers);
 
+const options = {
+  // letsencrypt로 받은 인증서 경로를 입력
+  ca: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/cert.pem"),
+};
+
+const server = http.createServer(app);
+
 server.listen(3000, () => {
   console.log("서버 연결 성공");
 });
+https.createServer(options, app).listen(443);
 
 export { server, app };

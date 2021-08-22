8a35a99b9c8f24260c7f87fbf31ef92378fd43c2
soujinko <soujinko@naver.com>
2021-08-23 04:14:21 +0900

    https 주석처리

--- index.js
+++ index.js
@@ -11,27 +11,29 @@ import passport from "passport";
 import passportConfig from "./passport/passport.js";
 import swaggerDocs from "./config/swagger_config.js";
 import fs from "fs";
-import https from "https";
+// import https from "https";
+import http from "http";
 import verification from "./middleware/verification.js";
 import keepAlive from "./models/scripts/procedures_events.js";
-import Redis from "ioredis";
+// import Redis from "ioredis";
 import logger from "./config/winston_config.js";
 
 dotenv.config();
 
 const app = express();
+// app.use(express.static("public"));
+// const options = {
+//   // letsencrypt로 받은 인증서 경로를 입력
+//   ca: fs.readFileSync("/etc/letsencrypt/live/ruzan.shop/fullchain.pem"),
+//   key: fs.readFileSync("/etc/letsencrypt/live/ruzan.shop/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/ruzan.shop/cert.pem"),
+//   // requestCert: false,
+//   // rejectUnauthorized: false,
+// };
 
-const options = {
-  // letsencrypt로 받은 인증서 경로를 입력
-  ca: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/fullchain.pem"),
-  key: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/soujinko.shop/cert.pem"),
-  requestCert: false,
-  rejectUnauthorized: false,
-};
-
-const server = https.createServer(options, app);
-const redisClient = new Redis({ password: process.env.REDIS_PASSWORD });
+// const server = https.createServer(options, app);
+const server = http.createServer(app);
+// const redisClient = new Redis({ password: process.env.REDIS_PASSWORD });
 
 const corsOption = {
   origin: [
@@ -44,7 +46,6 @@ const corsOption = {
   optionSuccessStatus: 200,
 };
 
-app.use(express.static("public"));
 app.use(cors(corsOption));
 app.use(morgan("combined", { stream: logger.stream }));
 // 헬멧은 기본적으로 15가지 보안 기능 중 11가지 기능을 제공하고, 4가지 기능은 명시적으로 사용을 지정해야한다.
@@ -55,7 +56,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(csrfProtection({ cookie: true })); // csrfProtection은 cookieparser나 session미들웨어보다 밑에 있어야한다.
 app.use(passport.initialize());
 passportConfig();
-app.use(/^((?!users).)*$/, verification);
+// app.use(/^((?!users).)*$/, verification);
 
 app.use("/api", router);
 app.use("/docs", swaggerDocs);
@@ -63,4 +64,4 @@ app.use(errorHandlers);
 
 setInterval(keepAlive, 60 * 240 * 1000);
 
-export { server, redisClient };
+export { server };

850ec1f54d09769610cdb7f99a48a6173879bec6
soujinko <soujinko@naver.com>
2021-08-24 23:48:00 +0900

    모델 보완

--- models/likes.js
+++ models/likes.js
@@ -1,3 +1,3 @@
-const likes = `CREATE TABLE if not exists likes (targetPk VARCHAR(20) NOT NULL,userPk INT UNSIGNED NOT NULL,INDEX fk_likes_users_idx (userPk ASC) VISIBLE,CONSTRAINT fk_likes_users FOREIGN KEY (userPk) REFERENCES hang.users (userPk) ON DELETE NO ACTION ON UPDATE NO ACTION) ENGINE = InnoDB;`;
+const likes = `CREATE TABLE if not exists likes (targetPk VARCHAR(20) NOT NULL,userPk INT UNSIGNED NOT NULL,INDEX fk_likes_users_idx (userPk ASC) VISIBLE,CONSTRAINT fk_likes_users FOREIGN KEY (userPk) REFERENCES hang.users (userPk) ON DELETE CASCADE ON UPDATE NO ACTION) ENGINE = InnoDB;`;
 
 export default likes;

1702da0c4201935cbd5699a28be3273968f364b4
soujinko <soujinko@naver.com>
2021-08-31 14:48:37 +0900

    update redis cache

--- functions/req_look_aside.js
+++ functions/req_look_aside.js
@@ -1,5 +1,5 @@
 // import redis from "../config/redis.cluster.config.js";
-import redis from '../config/redis.cluster.config.js'
+import redis from "../config/redis.cluster.config.js";
 
 const checkMypageRedis = (req, res, next) => {
   const { userPk } = res.locals.user;
@@ -8,39 +8,30 @@ const checkMypageRedis = (req, res, next) => {
   redis.hget(`mypage-${pagePk}`, "userInfo", function (error, userInfo) {
     if (error) next();
     if (userInfo) {
-      redis.hget(
-        `mypage-${pagePk}`,
-        "tripInfo",
-        function (error, tripInfo) {
-          if (error) next();
-          if (tripInfo) {
-            tripInfo = JSON.parse(tripInfo);
-            userInfo = JSON.parse(userInfo);
-            if (parseInt(userPk) === parseInt(pagePk)) {
-              if (Object.keys(userInfo).includes("like"))
-                delete userInfo["like"];
+      redis.hget(`mypage-${pagePk}`, "tripInfo", function (error, tripInfo) {
+        if (error) next();
+        if (tripInfo) {
+          tripInfo = JSON.parse(tripInfo);
+          userInfo = JSON.parse(userInfo);
+          if (parseInt(userPk) === parseInt(pagePk)) {
+            if (Object.keys(userInfo).includes("like")) delete userInfo["like"];
 
-              res.send({ userInfo, tripInfo });
-            } else {
-              redis.hget(
-                `mypage-${userPk}`,
-                "likes",
-                function (error, likes) {
-                  // console.log("likes-유저페이지", likes);
-                  if (error) next();
-                  if (likes) {
-                    const likes2 = JSON.parse(likes);
-                    userInfo.like = likes2.includes(parseInt(pagePk))
-                      ? true
-                      : false;
-                    res.send({ userInfo, tripInfo });
-                  } else next();
-                }
-              );
-            }
-          } else next();
-        }
-      );
+            res.send({ userInfo, tripInfo });
+          } else {
+            redis.hget(`mypage-${userPk}`, "likes", function (error, likes) {
+              // console.log("likes-유저페이지", likes);
+              if (error) next();
+              if (likes) {
+                const likes2 = JSON.parse(likes);
+                userInfo.like = likes2.includes(parseInt(pagePk))
+                  ? true
+                  : false;
+                res.send({ userInfo, tripInfo });
+              } else next();
+            });
+          }
+        } else next();
+      });
     } else next();
   });
 };

370706ffac9b99eaa8d3ec0ec0dea06693c51ba6
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-09-01 04:01:11 +0900

    Update error_handlers.js

--- util/error_handlers.js
+++ util/error_handlers.js
@@ -1,6 +1,6 @@
 import express from "express";
 import createError from "http-errors";
-// import logger from "../config/winston_config.js";
+import logger from "../config/winston_config.js";
 
 const router = express.Router();
 
@@ -11,25 +11,32 @@ router.use(function (req, res, next) {
 // csrf error handle
 router.use(function (err, req, res, next) {
   if (err.code !== "EBADCSRFTOKEN") return next(err);
+  logger.error(
+    `
+    ${err.status || 500} - 
+    ${err.message} - 
+    ${req.originalUrl} - 
+    ${req.method} - 
+    ${req.ip}
+    `
+  );
 
-  console.error(err);
-  res.status(403);
-  res.send({ response: err });
+  res.status(403).send({ response: err });
 });
 
 // error handler
 router.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
   logger.error(
-    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
-      req.method
-    } - ${req.ip}`
+    `
+    ${err.status || 500} - 
+    ${err.message} - 
+    ${req.originalUrl} - 
+    ${req.method} - 
+    ${req.ip}
+    `
   );
-  // render the error page
-  res.status(err.status || 500).json({ message: res.locals.message });
+  
+  res.status(err.status || 500).json({ message: err.message });
 });
 
 export default router;

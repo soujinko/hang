78788b2193ac30eb1292bac352d88c1863df8ecc
soujinko <soujinko@naver.com>
2021-08-31 14:49:05 +0900

    tag column 추가

--- models/index.js
+++ models/index.js
@@ -3,8 +3,6 @@ import users from "./users.js";
 import trips from "./trips.js";
 import likes from "./likes.js";
 import requests from "./requests.js";
-// import alarms from "./alarms.js";
-import auth from "./auth.js";
 
 // 이 파일을 실행해서 db를 세팅하세요
 async function createDb() {
@@ -13,7 +11,6 @@ async function createDb() {
   await connection.query(trips);
   await connection.query(likes);
   await connection.query(requests);
-  await connection.query(auth);
   await connection.query(
     "create view userView as select userPk, nickname, userId, region, city, age, gender,guide, profileImg,intro, tags from hang.users"
   );

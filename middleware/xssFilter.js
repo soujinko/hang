ef097ae377899f158b22e065df25a893dd2d7414
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-09-02 03:06:52 +0900

    xssFilter에 0 건너뛰기 추가

--- middleware/xssFilter.js
+++ middleware/xssFilter.js
@@ -7,7 +7,8 @@ const { sanitize } = DOMPurify(window)
 const purifier = async(body) => {
   if (!body) return
   for (let prop of Object.keys(body)) {
-    if (typeof prop === 'string') body[prop] = await sanitize(req.body[prop])
+    if (typeof prop === 'string') 
+      if (body[prop] !== '0') body[prop] = await sanitize(req.body[prop])
     else if (typeof prop === 'object') {
       body[prop] = await purifier(prop)
     }
@@ -20,44 +21,5 @@ const xssFilter = async(req, res, next) => {
   if (sanitized) req.body = sanitized
   next()
 }
-
-// let req = {body:{1:`
-//         hell <script>alert("hi");</script>
-//         <div onclick="alert(123);">
-//                 o
-//         </div>
-//         world
-//         <img id="createElement">
-// `,2:{1:`
-//         hell <script>alert("hi");</script>
-//         <div onclick="alert(123);">
-//                 o
-//         </div>
-//         world
-//         <img id="createElement">
-// `,2:`
-//         hell <script>alert("hi");</script>
-//         <div onclick="alert(123);">
-//                 o
-//         </div>
-//         world
-//         <img id="createElement">
-// `},3:[`
-//         hell <script>alert("hi");</script>
-//         <div onclick="alert(123);">
-//                 o
-//         </div>
-//         world
-//         <img id="createElement">
-// `,`
-//         hell <script>alert("hi");</script>
-//         <div onclick="alert(123);">
-//                 o
-//         </div>
-//         world
-//         <img id="createElement">
-// `]}}
-
-// console.log(await xssFilter(req))
 
 export default xssFilter

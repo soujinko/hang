13d8c2976db82cfb0c75ff29d6653e93bcb95e7f
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-09-07 00:31:20 +0900

    DEL unused error handler

--- util/error_handlers.js
+++ util/error_handlers.js
@@ -8,22 +8,6 @@ router.use(function (req, res, next) {
   next(createError(404));
 });
 
-// csrf error handle
-// router.use(function (err, req, res, next) {
-//   if (err.code !== "EBADCSRFTOKEN") return next(err);
-//   logger.error(
-//     `
-//     ${err.status || 500} - 
-//     ${err.message} - 
-//     ${req.originalUrl} - 
-//     ${req.method} - 
-//     ${req.ip}
-//     `
-//   );
-
-//   res.status(403).send({ response: err });
-// });
-
 // error handler
 router.use(function (err, req, res, next) {
   logger.error(
